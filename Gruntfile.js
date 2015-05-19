915ec69dd0c4624c4dbb6429b2f4e5ebfc0f0ea9
David Reiher <dr@thepeaklab.com>
2015-05-19 12:05:08 +0200

    fix(grunt): concat in order so the template.js will be included at the end of the js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
         stripBanners: true
       },
       js: {
-        src: ['<%= yo.src %>/*.js'],
+        src: ['<%= yo.src %>/*.js', '!<%= yo.src %>/templates.js', '<%= yo.src %>/templates.js',],
         dest: '<%= yo.dist %>/tpl-table.js'
       },
       css: {
