91452e84e13f4af3ae1e12611ce9488e25a43fc9
Chris Eli <chris.elies13@gmail.com>
2016-06-29 00:43:37 +0200

    fix(): file name in barrel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from './filter';
 export * from './helper';
 export * from './interfaces';
 export * from './loading-points';
+export * from './pipes';
 export * from './tpl-table.component';
 export * from './tpl-table.service';
