49a4ae8ec9be2366978d31dca17b0395985281c7
David Reiher <dr@thepeaklab.com>
2016-07-07 14:06:19 +0200

    feat(editCallback): added callback when cell values are edited

--- dist/tpl-table.js
+++ dist/tpl-table.js
@@ -130,7 +130,7 @@
         vm.opts.pageAndSearchChangeMethod = vm.opts.pageAndSearchChangeMethod || function () {
           $log.info('tbl-table: no pageAndSearchChangeMethod given');
         };
-        vm.opts.onColumnEdit = vm.opts.onColumnEdit || null;
+        vm.opts.onCellEdit = vm.opts.onCellEdit || null;
         vm.opts.columns = vm.opts.columns || [
           {
             name: '',
@@ -379,7 +379,7 @@
         vm.editableCell[0] = null;
         vm.editableCell[1] = null;
         // given values are the rowId and the data holded by the row
-        vm.opts.onColumnEdit(vm.editableCell[0], vm.opts.entries[vm.editableCell[0]]);
+        vm.opts.onCellEdit(vm.editableCell[0], vm.opts.entries[vm.editableCell[0]]);
       };
       function getCellValue(row, cell) {
         var levels = cell.split('.');
